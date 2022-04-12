b0156f51ff455ce1376b6be213577fd38807bdaa
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-04-12 16:15:55 -0400

    MongoDB conncected, new products are able to be pushed to the database, confirmed useing Postman

--- models/product.js
+++ models/product.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength:1, maxlength:25},
     description: {type: String, required: true, minlength:1},
     category: {type: String, required: true, minlength:1, maxlength:25},
     price: {type: Number, required: true},
-    datAdded: {type: Date, default: Date.now()},
+    dateAdded: {type: Date, default: Date.now()},
 });
+
+function validateProduct(product){
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(255).required(),
+        description: Joi.string().required(),
+        category: Joi.string().min(2).max(255).required(),
+        price: Joi.number().required()
+    })
+    return schema.validate(product);
+}
+
 
 const Product = mongoose.model('Product', productSchema);
 
-module.export = Product;
+module.exports = {
+    Product,
+    validateProduct
+};
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,20 @@
-const Product = require("../models/product");
+const {Product, validateProduct} = require("../models/product");
 const express = require("express");
 const router = express.Router();
 
-
+router.post("/", async (req, res) => {
+    try {
+        const {error} = validateProduct(req.body)
+        if (error) return res.status(400).send(error);
+       
+        let newProduct = await new Product(req.body)
+        await newProduct.save()
+        
+        return res.status(201).send(newProduct)
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
 
 
 module.exports = router;
