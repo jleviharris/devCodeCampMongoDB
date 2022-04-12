7ac16d3bc1c4a6fe2888e451e1f4306a179c2ced
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-04-12 17:07:29 -0400

    GET product by id working, confirmed using Postman

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,34 @@ const {Product, validateProduct} = require("../models/product");
 const express = require("express");
 const router = express.Router();
 
+// GET all products
+// http://localhost:3007/api/products
+router.get("/", async (req, res) => {
+    try {
+        let products = await Product.find();
+        if (!products) return res.status(400).send(`No products in this collection!`);
+        return res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
+
+//GET a product by id
+// http://localhost:3007/api/products/:productId
+router.get("/:productId", async (req,res) => {
+    try {
+        let product = await Product.findById(req.params.productId)
+        if (!product)
+            return res.status(400).send(`Product with Id of ${req.params.productId} does not exist!`);
+        return res.status(200).send(product);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+})
+
+//POST a new product
+// http://localhost:3007/api/products
+
 router.post("/", async (req, res) => {
     try {
         const {error} = validateProduct(req.body)
